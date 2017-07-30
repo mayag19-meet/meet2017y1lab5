68ae590b73d27e6d783491df730db5cf523bc725
mayag19-meet <mayag19@meet.mit.edu>
2017-07-30 10:55:17 +0200

    Stopping here

---
+++ fun2.py
@@ -0,0 +1,11 @@
+#def draw_1d(n, char):
+    #print(char*n)
+
+#def draw_rectangle(n1,n2):
+    #for i in range(n1):
+        #print("*"*n2)
+    
+
+def draw_2d(n, m, char):
+    for i in range(n):
+        print(char*m)
